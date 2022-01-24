5ef6b84b8506f5afa99effaf96fcffa16d14da06
aprouco <adripenasrouco@gmail.com>
2022-01-24 16:50:10 +0100

    Ejercicio8

---
+++ ejercicio9.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio9</title>
+</head>
+<body>
+    <?php
+    $EUROS=0.0060;
+    $moneda=6000;
+    $conversion=$moneda * $EUROS;
+    echo "$moneda "."pesetas son ".$conversion." euros";
+    ?>
+</body>
+</html>
